63df95dde8353c3b2cf3654212e45baf9bfd6e6c
remax-space <carlosalvesbasile@gmail.com>
2026-06-01 15:32:49 -0300

    sw: bump para v103 forçar limpeza cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'remax-space-v48';
+const CACHE = 'remax-space-v103';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(
